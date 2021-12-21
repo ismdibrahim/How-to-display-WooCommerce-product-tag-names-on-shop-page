308aac5681de2080e1b735b8c541cc5e4b47f117
Md. Ibrahim Hossen <48528670+ismdibrahim@users.noreply.github.com>
2021-12-22 00:11:41 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,9 +3,15 @@ add_action( 'woocommerce_before_shop_loop_item_title', 'aq_display_brand_before_
    global $product;
    $product_id = $product->get_id();
    $brands = wp_get_post_terms( $product_id, 'product_tag' );
-   foreach( $brands as $brand ){
-     echo '<a href="'.get_term_link($brand->term_id).'" class="category uppercase is-smaller no-text-overflow product-cat op-7">';
-      echo $brand->name;
-     echo '</a>';
-   }
+   ?>
+     <div class="products-tag">
+   <?php
+    foreach( $brands as $brand ){
+      echo '<a href="'.get_term_link($brand->term_id).'" class="tag">';
+       echo $brand->name;
+      echo '</a>';
+    }
+   ?>
+    </div>
+   <?php
  }
